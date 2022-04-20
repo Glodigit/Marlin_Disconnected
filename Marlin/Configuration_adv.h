70add30a43f135628b0360e34f40787a05d06709
Glodigit <kdengelin@hotmail.com>
2022-04-20 17:28:32 +0100

    Set E to spreadcycle and 1/8 microstepping

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2888,7 +2888,7 @@
 
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT      800
-    #define E0_MICROSTEPS    16
+    #define E0_MICROSTEPS    8
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
     //#define E0_INTERPOLATE true
@@ -3051,7 +3051,7 @@
   #define STEALTHCHOP_U
   #define STEALTHCHOP_V
   #define STEALTHCHOP_W
-  #define STEALTHCHOP_E
+  //#define STEALTHCHOP_E
 
   /**
    * Optimize spreadCycle chopper parameters by using predefined parameter sets

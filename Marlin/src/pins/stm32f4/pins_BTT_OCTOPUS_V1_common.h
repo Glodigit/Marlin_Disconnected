89c461673cdbaeb96674c6bcaf14905272fd9926
Glodigit <kdengelin@hotmail.com>
2024-02-11 06:03:22 +0000

    Correct endstop pins for XYZ on EXP MOT

--- Marlin/src/pins/stm32f4/pins_BTT_OCTOPUS_V1_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_OCTOPUS_V1_common.h
@@ -47,58 +47,114 @@
 //
 // Limit Switches
 //
-#ifdef X_STALL_SENSITIVITY
-  #define X_STOP_PIN                  X_DIAG_PIN
-  #if X_HOME_TO_MIN
-    #define X_MAX_PIN                E0_DIAG_PIN  // E0DET
+#if ENABLED(XYZ_ON_BTT_MOTOR_EXPANSION)
+  #ifdef X_STALL_SENSITIVITY
+    #define X_STOP_PIN                  X_DIAG_PIN
+    #if X_HOME_TO_MIN
+      #define X_MAX_PIN                E4_DIAG_PIN
+    #else
+      #define X_MIN_PIN                E4_DIAG_PIN
+    #endif
+  #elif NEEDS_X_MINMAX
+    #ifndef X_MIN_PIN
+      #define X_MIN_PIN                E0_DIAG_PIN
+    #endif
+    #ifndef X_MAX_PIN
+      #define X_MAX_PIN                E4_DIAG_PIN
+    #endif
   #else
-    #define X_MIN_PIN                E0_DIAG_PIN  // E0DET
+    #define X_STOP_PIN                 E0_DIAG_PIN
   #endif
-#elif NEEDS_X_MINMAX
-  #ifndef X_MIN_PIN
-    #define X_MIN_PIN                 X_DIAG_PIN  // X-STOP
-  #endif
-  #ifndef X_MAX_PIN
-    #define X_MAX_PIN                E0_DIAG_PIN  // E0DET
-  #endif
-#else
-  #define X_STOP_PIN                  X_DIAG_PIN  // X-STOP
-#endif
 
-#ifdef Y_STALL_SENSITIVITY
-  #define Y_STOP_PIN                  Y_DIAG_PIN
-  #if Y_HOME_TO_MIN
-    #define Y_MAX_PIN                E1_DIAG_PIN  // E1DET
+  #ifdef Y_STALL_SENSITIVITY
+    #define Y_STOP_PIN                  Y_DIAG_PIN
+    #if Y_HOME_TO_MIN
+      #define Y_MAX_PIN                E5_DIAG_PIN 
+    #else
+      #define Y_MIN_PIN                E5_DIAG_PIN
+    #endif
+  #elif NEEDS_Y_MINMAX
+    #ifndef Y_MIN_PIN
+      #define Y_MIN_PIN                E1_DIAG_PIN
+    #endif
+    #ifndef Y_MAX_PIN
+      #define Y_MAX_PIN                E5_DIAG_PIN
+    #endif
   #else
-    #define Y_MIN_PIN                E1_DIAG_PIN  // E1DET
+    #define Y_STOP_PIN                 E1_DIAG_PIN
   #endif
-#elif NEEDS_Y_MINMAX
-  #ifndef Y_MIN_PIN
-    #define Y_MIN_PIN                 Y_DIAG_PIN  // Y-STOP
-  #endif
-  #ifndef Y_MAX_PIN
-    #define Y_MAX_PIN                E1_DIAG_PIN  // E1DET
-  #endif
-#else
-  #define Y_STOP_PIN                  Y_DIAG_PIN  // Y-STOP
-#endif
 
-#ifdef Z_STALL_SENSITIVITY
-  #define Z_STOP_PIN                  Z_DIAG_PIN
-  #if Z_HOME_TO_MIN
-    #define Z_MAX_PIN                E2_DIAG_PIN  // PWRDET
+  #ifdef Z_STALL_SENSITIVITY
+    #define Z_STOP_PIN                  Z_DIAG_PIN
+    #if Z_HOME_TO_MIN
+      #define Z_MAX_PIN                E6_DIAG_PIN
+    #else
+      #define Z_MIN_PIN                E6_DIAG_PIN 
+    #endif
+  #elif NEEDS_Z_MINMAX
+    #ifndef Z_MIN_PIN
+      #define Z_MIN_PIN                E2_DIAG_PIN
+    #endif
+    #ifndef Z_MAX_PIN
+      #define Z_MAX_PIN                E6_DIAG_PIN
+    #endif
   #else
-    #define Z_MIN_PIN                E2_DIAG_PIN  // PWRDET
-  #endif
-#elif NEEDS_Z_MINMAX
-  #ifndef Z_MIN_PIN
-    #define Z_MIN_PIN                 Z_DIAG_PIN  // Z-STOP
-  #endif
-  #ifndef Z_MAX_PIN
-    #define Z_MAX_PIN                E2_DIAG_PIN  // PWRDET
+    #define Z_STOP_PIN                 E2_DIAG_PIN
   #endif
 #else
-  #define Z_STOP_PIN                  Z_DIAG_PIN  // Z-STOP
+  #ifdef X_STALL_SENSITIVITY
+    #define X_STOP_PIN                  X_DIAG_PIN
+    #if X_HOME_TO_MIN
+      #define X_MAX_PIN                E0_DIAG_PIN  // E0DET
+    #else
+      #define X_MIN_PIN                E0_DIAG_PIN  // E0DET
+    #endif
+  #elif NEEDS_X_MINMAX
+    #ifndef X_MIN_PIN
+      #define X_MIN_PIN                 X_DIAG_PIN  // X-STOP
+    #endif
+    #ifndef X_MAX_PIN
+      #define X_MAX_PIN                E0_DIAG_PIN  // E0DET
+    #endif
+  #else
+    #define X_STOP_PIN                  X_DIAG_PIN  // X-STOP
+  #endif
+
+  #ifdef Y_STALL_SENSITIVITY
+    #define Y_STOP_PIN                  Y_DIAG_PIN
+    #if Y_HOME_TO_MIN
+      #define Y_MAX_PIN                E1_DIAG_PIN  // E1DET
+    #else
+      #define Y_MIN_PIN                E1_DIAG_PIN  // E1DET
+    #endif
+  #elif NEEDS_Y_MINMAX
+    #ifndef Y_MIN_PIN
+      #define Y_MIN_PIN                 Y_DIAG_PIN  // Y-STOP
+    #endif
+    #ifndef Y_MAX_PIN
+      #define Y_MAX_PIN                E1_DIAG_PIN  // E1DET
+    #endif
+  #else
+    #define Y_STOP_PIN                  Y_DIAG_PIN  // Y-STOP
+  #endif
+
+  #ifdef Z_STALL_SENSITIVITY
+    #define Z_STOP_PIN                  Z_DIAG_PIN
+    #if Z_HOME_TO_MIN
+      #define Z_MAX_PIN                E2_DIAG_PIN  // PWRDET
+    #else
+      #define Z_MIN_PIN                E2_DIAG_PIN  // PWRDET
+    #endif
+  #elif NEEDS_Z_MINMAX
+    #ifndef Z_MIN_PIN
+      #define Z_MIN_PIN                 Z_DIAG_PIN  // Z-STOP
+    #endif
+    #ifndef Z_MAX_PIN
+      #define Z_MAX_PIN                E2_DIAG_PIN  // PWRDET
+    #endif
+  #else
+    #define Z_STOP_PIN                  Z_DIAG_PIN  // Z-STOP
+  #endif
 #endif
 
 //

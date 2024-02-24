51f9fe1b4de5bb1acce425d9473707608d382559
Glodigit <kdengelin@hotmail.com>
2024-02-24 09:58:46 +0000

    config tweaks

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -710,8 +710,8 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 248
-#define HEATER_1_MAXTEMP 275
+#define HEATER_0_MAXTEMP 320
+#define HEATER_1_MAXTEMP 248
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
 #define HEATER_4_MAXTEMP 275
